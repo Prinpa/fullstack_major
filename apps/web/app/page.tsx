cc0c26236ce7bb164bee0ca00f8f39fe947a62e8
Pat Dowd <pa77y04@gmail.com>
2025-06-05 12:35:34 +1000

    still testing something

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,15 +6,13 @@ import  { Product } from "types";
 import { FilterForm } from "components/Products/filterForm";
 
 export default async function Home() {
-  const initialProducts = await getProducts();
+  //const initialProducts = await getProducts();
   
   return (
-    <AppLayout>
       <div className="flex flex-col gap-4">
           <div className="p-4">
             <h1 className="text-2xl font-bold">Welcome to Our Store</h1>
           </div>
       </div>
-    </AppLayout>
   );
 }
